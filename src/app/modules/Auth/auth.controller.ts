a4316068a60871f33cb7b390b08df5d5b0c0ced6
jubayer44 <jubayerahmedshamim44@gmail.com>
2024-06-05 17:06:05 +0600

    updated-project

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -23,8 +23,6 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 const refreshToken = catchAsync(async (req: Request, res: Response) => {
   const  refreshToken = req.cookies?.refreshToken;
 
-  console.log(req)
-
   const result = await AuthServices.refreshToken(refreshToken);
 
   sendResponse(res, {
